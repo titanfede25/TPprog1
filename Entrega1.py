2d90a534999a5a00d1a0b123ca148fdc6018f9f4
titanfede25 <111387471+titanfede25@users.noreply.github.com>
2025-10-09 14:37:11 -0300

    Avanzando un poco con el menú

--- Entrega1.py
+++ Entrega1.py
@@ -1,10 +1,10 @@
 """
 -----------------------------------------------------------------------------------------------
 Título: Entrega 1 del TP grupal
-Fecha:
+Fecha: Hecho para el 22/10/2025
 Autor: Equipo 1. Federico Sznajderhaus, Benjamín Moyano, Samuel Franco Salazar, Galo Barus.
 
-Descripción:
+Descripción: Un club deportivo con actividades aranceladas necesita el desarrollo de una aplicación para informatizar la gestión de los pagos de los socios por cada deporte.
 
 Pendientes:
 -----------------------------------------------------------------------------------------------
@@ -71,12 +71,12 @@ def main():
                     opciones = 4
                     print()
                     print("---------------------------")
-                    print("MENÚ PRINCIPAL > MENÚ DE CLIENTES")
+                    print("MENÚ PRINCIPAL > MENÚ DE SOCIOS")
                     print("---------------------------")
-                    print("[1] Ingresar clientes")
-                    print("[2] Opción 2")
-                    print("[3] Opción 3")
-                    print("[4] Opción 4")
+                    print("[1] Ingresar socio")
+                    print("[2] Buscar socio/s")
+                    print("[3] Modificar socio")
+                    print("[4] Eliminar socio")
                     print("---------------------------")
                     print("[0] Volver al menú anterior")
                     print("---------------------------")
@@ -109,17 +109,89 @@ def main():
 
 
         elif opcionMenuPrincipal == "2":   # Opción 2 del menú principal
-            ...
+            while True:
+                while True:
+                    opciones = 4
+                    print()
+                    print("---------------------------")
+                    print("MENÚ PRINCIPAL > MENÚ DE DEPORTES")
+                    print("---------------------------")
+                    print("[1] Ingresar deporte")
+                    print("[2] Buscar deporte/s")
+                    print("[3] Modificar deporte")
+                    print("[4] Eliminar deporte")
+                    print("---------------------------")
+                    print("[0] Volver al menú anterior")
+                    print("---------------------------")
+                    print()
+                    
+                    opcionSubmenu = input("Seleccione una opción: ")
+                    if opcionSubmenu in [str(i) for i in range(0, opciones + 1)]: # Sólo continua si se elije una opcion de menú válida
+                        break
+                    else:
+                        input("Opción inválida. Presione ENTER para volver a seleccionar.")
+                print()
+
+                if opcionSubmenu == "0": # Opción salir del submenú
+                    break # No sale del programa, sino que vuelve al menú anterior
+                
+                elif opcionSubmenu == "1":   # Opción 1 del submenú
+                    ...
+                    
+                elif opcionSubmenu == "2":   # Opción 2 del submenú
+                    ...
+                
+                elif opcionSubmenu == "3":   # Opción 3 del submenú
+                    ...
+                
+                elif opcionSubmenu == "4":   # Opción 4 del submenú
+                    ...
+
+                input("\nPresione ENTER para volver al menú.") # Pausa entre opciones
+                print("\n\n")
+
+
         
         elif opcionMenuPrincipal == "3":   # Opción 3 del menú principal
-            ...
-        
+            while True:
+                while True:
+                    opciones = 2
+                    print()
+                    print("---------------------------")
+                    print("MENÚ PRINCIPAL > MENÚ DE PAGOS")
+                    print("---------------------------")
+                    print("[1] Ingresar pago")
+                    print("[2] Eliminar pago")
+                    print("---------------------------")
+                    print("[0] Volver al menú anterior")
+                    print("---------------------------")
+                    print()
+                    
+                    opcionSubmenu = input("Seleccione una opción: ")
+                    if opcionSubmenu in [str(i) for i in range(0, opciones + 1)]: # Sólo continua si se elije una opcion de menú válida
+                        break
+                    else:
+                        input("Opción inválida. Presione ENTER para volver a seleccionar.")
+                print()
+
+                if opcionSubmenu == "0": # Opción salir del submenú
+                    break # No sale del programa, sino que vuelve al menú anterior
+                
+                elif opcionSubmenu == "1":   # Opción 1 del submenú
+                    ...
+                    
+                elif opcionSubmenu == "2":   # Opción 2 del submenú
+                    ...
+
+                input("\nPresione ENTER para volver al menú.") # Pausa entre opciones
+                print("\n\n")
+
+
+           
         elif opcionMenuPrincipal == "4":   # Opción 4 del menú principal
             ...
 
-        elif opcionMenuPrincipal == "5":   # Opción 5 del menú principal
-            ...
-
+        
         if opcionSubmenu != "0": # Pausa entre opciones. No la realiza si se vuelve de un submenú
             input("\nPresione ENTER para volver al menú.")
             print("\n\n")
